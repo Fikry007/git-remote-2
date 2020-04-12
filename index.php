0744a11215beb678cef3a8c48742b87041790ea5
Fikry <fikrysolot@gmail.com>
2020-04-12 18:16:37 +0700

    Menambahkan List

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Git Remote 2</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fugiat perferendis eum consectetur, veniam, aperiam quae labore quo eveniet nam aspernatur nostrum facere dolorum saepe vero excepturi, nesciunt eos iure.</p>
+    <ul>
+    <li>Satu</li>
+    <li>Dua</li>
+    </ul>
 </body>
 </html>
